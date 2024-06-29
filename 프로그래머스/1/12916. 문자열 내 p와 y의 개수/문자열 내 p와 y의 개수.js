2c4045d0d20ed20de00a29eda99459e31cabc572
ExpeditionMoon <128493900+ExpeditionMoon@users.noreply.github.com>
2024-06-29 14:07:57 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,10 +1,3 @@
 function solution(s){
-    let answer = true; 
-    const cnt = [0, 0];
-    s.toUpperCase().split('').forEach(alp => {
-        if (alp === 'P') cnt[0]++;
-        if (alp === 'Y') cnt[1]++;
-    })
-    if (cnt[0] !== cnt[1]) answer = false;
-    return answer;
+    return s.toUpperCase().split('P').length === s.toUpperCase().split('Y').length;
 }
